bdc472029a6ade0085433b826d863eb24a34b2ee
mizunowanko <siyou1315@gmail.com>
2015-10-15 21:21:19 +0900

    define exp and log change all method to prototype

--- Complex.js
+++ Complex.js
@@ -3,72 +3,136 @@
  **/
 
 
+//
+//
+//
+
+
 //Complex Class
 var Complex;
 Complex = function (re, im) {
+
 
     //real number
     this.re = re;
 
     //imaginary number
     this.im = im;
+};
 
-    //scala multiple
-    this.scl = function (k) {
-        return new Complex(this.re * k, this.im * k);
-    };
+//--------------------
+//     Object
+//--------------------
 
-    //abstract
-    this.abs = function () {
-        return Math.sqrt(this.abs2());
-    };
-
-    //square of abstract
-    this.abs2 = function () {
-        return Math.pow(this.re, 2) + Math.pow(this.im, 2);
-    };
-
-    //declination
-    this.arg = function () {
-        return Math.atan(this.im / this.re);
-    };
-
-    //conjgate
-    this.conj = function () {
-        return new Complex(this.re, -this.im);
-    };
-
-    //inverse
-    this.inv = function () {
-        var a = new Complex(this.re, -this.im);
-        return a.scl(1 / this.abs2());
-    };
+Complex.prototype.toString = function () {
+    return String(this.re.toFixed(2)) + " + " + String(this.im.toFixed(2)) + "i";
 };
 
 
+//--------------------
+//    basic operations
+//--------------------
+
+
+//scala multiple
+Complex.prototype.scl = function (k) {
+    return new Complex(this.re * k, this.im * k);
+};
+
+//abstract
+Complex.prototype.abs = function () {
+    return Math.sqrt(this.abs2());
+};
+
+//square of abstract
+Complex.prototype.abs2 = function () {
+    return Math.pow(this.re, 2) + Math.pow(this.im, 2);
+};
+
+//conjgate
+Complex.prototype.conj = function () {
+    return new Complex(this.re, -this.im);
+};
+
+//inverse
+Complex.prototype.inv = function () {
+    var a = new Complex(this.re, -this.im);
+    return a.scl(1 / this.abs2());
+};
+
+
+//--------------------------------
+//    Four arithmetic operations
+//--------------------------------
+
 //add
-Complex.add = function (a, b) {
-    return new Complex(a.re + b.re, a.im + b.im);
+Complex.prototype.add = function (that) {
+    return new Complex(this.re + that.re, this.im + that.im);
 };
 
 //subtract
-Complex.sub = function (a, b) {
-    return new Complex(a.re - b.re, a.im - b.im);
+Complex.prototype.sub = function (that) {
+    return new Complex(this.re - that.re, this.im - that.im);
 };
 
 //multiple
-Complex.mlt = function (a, b) {
-    return new Complex(a.re * b.re - a.im * b.im, a.re * b.im + a.im + b.re);
+Complex.prototype.mlt = function (that) {
+    return new Complex(this.re * that.re - this.im * that.im, this.re * that.im + this.im + that.re);
 };
 
 //division
-Complex.div = function (a, b) {
-    return new Complex.mlt(a, b.inv());
+Complex.prototype.div = function (that) {
+    return new this.mlt(that.inv());
+};
+
+//---------------------
+//    polar form
+//---------------------
+
+//declination
+Complex.prototype.arg = function () {
+    return this.re > 0 ? Math.atan(this.im / this.re) : Math.atan(this.im / this.re) + Math.PI;
+};
+
+//cos
+Complex.prototype.cos = function () {
+    return this.re / this.abs();
+};
+
+//sin
+Complex.prototype.sin = function () {
+    return this.im / this.abs();
 };
 
 
+//--------------------------------
+//    exponential & logarithm
+//--------------------------------
+
+//exponential
+Complex.prototype.exp = function () {
+    return Complex.expi(this.im).scl(Math.exp(this.re));
+};
+
+//exponential applied to imaginary number
+Complex.expi = function (x) {
+    return new Complex(Math.cos(x), Math.sin(x));
+};
+
+//logarithm
+Complex.prototype.log = function () {
+    return new Complex(Math.log(this.abs()), this.arg());
+};
+
+
+
+//------------------------
+//
+//------------------------
+
+
 window.onload = function () {
-    var a = new Complex(1, -1), b = new Complex(2, 3);
-    var c = Complex.div(a, b);
-    window.alert(Math.pow(2, -3.3));
+    var a = new Complex(-1, 0), b = new Complex(0, Math.PI);
+    var c = b.exp();
+    window.alert(c);
 };
