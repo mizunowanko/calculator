3fd98ceb01896ab39cb49dcd8468a2f271b2be0a
mizunowanko <siyou1315@gmail.com>
2015-10-15 13:48:00 +0900

    複素数の定義と絶対値関数の定義

--- Complex.js
+++ Complex.js
@@ -2,4 +2,25 @@
  * Created by takashima on 15/10/15.
  */
 
+var Complex = function(re, im){
+    this.re = re;
+    this.im = im;
+    this.getRe = function(){
+        return this.re;
+    };
+    this.getIm = function(){
+        return this.im;
+    };
+    this.abs = function(){
+        return Math.sqrt(this.abs2());
+    }
+    this.abs2 = function() {
+        return this.re ^ 2 + this.im ^ 2;
+    }
+};
 
+window.onload = function(){
+    var a = new Complex(1,-1);
+
+    alert(a.abs());
+};
