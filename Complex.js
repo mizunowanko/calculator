22fc55fba57801d3f6c616a64786de13aced1f38
mizunowanko <siyou1315@gmail.com>
2015-10-15 17:13:43 +0900

    複素数の定義と絶対値関数の定義

--- Complex.js
+++ Complex.js
@@ -2,25 +2,74 @@
  * Created by takashima on 15/10/15.
  */
 
-var Complex = function(re, im){
+
+
+//Complex Class
+var Complex;
+Complex = function (re, im) {
+
+    //real number
     this.re = re;
+
+    //imaginary number
     this.im = im;
-    this.getRe = function(){
-        return this.re;
+
+    //scala multiple
+    this.scl = function (k) {
+        return new Complex(this.re * k, this.im * k);
     };
-    this.getIm = function(){
-        return this.im;
-    };
-    this.abs = function(){
+
+    //abstract
+    this.abs = function () {
         return Math.sqrt(this.abs2());
-    }
-    this.abs2 = function() {
-        return this.re ^ 2 + this.im ^ 2;
-    }
+    };
+
+    //square of abstract
+    this.abs2 = function () {
+        return Math.pow(this.re, 2) + Math.pow(this.im, 2);
+    };
+
+    //declination
+    this.arg = function () {
+        return Math.atan(this.im / this.re);
+    };
+
+    //conjgate
+    this.conj = function () {
+        return new Complex(this.re, -this.im);
+    };
+
+    //inverse
+    this.inv = function () {
+        var a = new Complex(this.re, -this.im);
+        return a.scl(1 / this.abs2());
+    };
 };
 
-window.onload = function(){
-    var a = new Complex(1,-1);
 
-    alert(a.abs());
+//add
+Complex.add = function (a, b) {
+    return new Complex(a.re + b.re, a.im + b.im);
+};
+
+//subtract
+Complex.sub = function (a, b) {
+    return new Complex(a.re - b.re, a.im - b.im);
+};
+
+//multiple
+Complex.mlt = function (a, b) {
+    return new Complex(a.re * b.re - a.im * b.im, a.re * b.im + a.im + b.re);
+};
+
+//division
+Complex.div = function (a, b) {
+    return new Complex.mlt(a, b.inv());
+};
+
+
+window.onload = function () {
+    var a = new Complex(1, -1), b = new Complex(2, 3);
+    var c = Complex.div(a, b);
+    window.alert(Math.pow(2, -3.3));
 };
