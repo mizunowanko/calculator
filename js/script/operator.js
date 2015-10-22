d2ca9340a44553fb666a03ac0d9249604479d854
mizunowanko <siyou1315@gmail.com>
2015-10-22 23:29:03 +0900

    ショートカットの変更後初めて

---
+++ js/script/operator.js
@@ -0,0 +1,33 @@
+/**
+ * Created by takashima on 15/10/17.
+ */
+
+
+G.script.Operator= function(name, priority, applying){
+    this.name = name;
+    this.priority = priority;
+    this.applying = applying;
+};
+
+
+//全ての演算子の配列
+G.script.Operator.Operators = [
+    new G.script.Operator("+", 2),
+    new G.script.Operator("-", 2),
+    new G.script.Operator("exp", 4),
+    new G.script.Operator("*", 6),
+    new G.script.Operator("/", 6),
+    new G.script.Operator("^", 8)
+];
+
+
+//特定の優先順位の演算子を全て格納した配列を返す
+G.script.Operator.filterByPriority = function (pri){
+    var a = G.script.Operator.Operators.filter(function(x){return x.priority === pri});
+    return a;
+};
+
+
+
+
+
