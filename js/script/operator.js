5794c230622668371217d483255545f6ed529de8
mizunowanko <siyou1315@gmail.com>
2015-10-27 23:37:41 +0900

    Operatorを名前空間を使って書き換える

--- js/script/operator.js
+++ js/script/operator.js
@@ -2,29 +2,33 @@
  * Created by takashima on 15/10/17.
  */
 
+(function(){
 
-G.script.Operator= function(name, priority, applying){
-    this.name = name;
-    this.priority = priority;
-    this.applying = applying;
-};
+    G.script.Operator = function(name, priority, applying){
+        this.name = name;
+        this.priority = priority;
+        this.applying = applying;
+    };
 
+    var Opr = G.script.Operator;
+    var Cmp = G.script.Complex;
 
 //全ての演算子の配列
-G.script.Operator.Operators = [
-    new G.script.Operator("+", 2),
-    new G.script.Operator("-", 2),
-    new G.script.Operator("log", 4),
-    new G.script.Operator("*", 6),
-    new G.script.Operator("/", 6),
-    new G.script.Operator("^", 8)
-];
+    Opr.operators = [
+        new Opr("+", 2, Cmp.add),
+        new Opr("-", 2, Cmp.sub),
+        new Opr("log", 4, Cmp.log),
+        new Opr("*", 6, Cmp.mlt),
+        new Opr("/", 6, Cmp.div),
+        new Opr("^", 8, Cmp.pow)
+    ];
 
 
 //特定の優先順位の演算子を全て格納した配列を返す
-G.script.Operator.filterByPriority = function (pri){
-    var a = G.script.Operator.Operators.filter(function(x){return x.priority === pri});
-    return a;
-};
-
-
+    Opr.filterByPriority = function(pri){
+        var a = Opr.operators.filter(function(x){
+            return x.priority === pri
+        });
+        return a;
+    };
+}());
--- js/test/test_complex.js
+++ js/test/test_complex.js
@@ -12,8 +12,8 @@ G.test.TestComplex.testExp = function(){
 };
 
 G.test.TestComplex.testPow = function(){
-    var a = new G.script.Complex(Math.E, 0);
-    var b = new G.script.Complex(0, Math.PI);
+    var a = new G.script.Complex(2, 0);
+    var b = new G.script.Complex(1, 1);
     alert(G.script.Complex.pow(a, b));
 };
 
